0a8dc54a5237f2552d2996072a2adaeddcdd4ed6
marcobonici <bonici.marco@gmail.com>
2025-04-19 13:07:23 -0400

    Fixed typo

--- Effort.jl/mnuw0wacdm/job_effort_pybird_mnuw0wacdm.sh
+++ Effort.jl/mnuw0wacdm/job_effort_pybird_mnuw0wacdm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/mbonici/julia-1.9.1/bin/julia /home/mbonici/EmulatorsTrainer.jl/scripts/effort_pybird_script_mnuw0wacdm.jl
+/home/mbonici/julia-1.9.1/bin/julia /home/mbonici/emulator-zoo/Effort.jl/mnuw0wacdm/effort_pybird_script_mnuw0wacdm.jl
