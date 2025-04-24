c803f8758d4ba3652c6ee3155e490ddd985335fe
marcobonici <bonici.marco@gmail.com>
2025-04-24 17:22:22 -0400

    Fixed postprocessing file

--- Effort.jl/velocileptors_lcdm/postprocessing_loop.py
+++ Effort.jl/velocileptors_lcdm/postprocessing_loop.py
@@ -1,4 +1,4 @@
 import jax.numpy as jnp
 
-def postprocessing(input, Pl, D):
-    return Pl * (jnp.exp(input[1])*1e-10 * D**2)**2
+def postprocessing(input, Pl):
+    return Pl * (jnp.exp(input[0])*1e-10)**2
--- Effort.jl/velocileptors_lcdm/postprocessing_st.py
+++ Effort.jl/velocileptors_lcdm/postprocessing_st.py
@@ -1,4 +1,4 @@
 import jax.numpy as jnp
 
-def postprocessing(input, Pl, D):
+def postprocessing(input, Pl):
     return Pl
