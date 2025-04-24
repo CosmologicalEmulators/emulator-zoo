3dcb860b19016b7dab13978d66ca4d802abdce86
marcobonici <bonici.marco@gmail.com>
2025-04-24 17:26:52 -0400

    FIle renaming

---
+++ Effort.jl/velocileptors_lcdm/job_data_generation.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/mbonici/julia-1.9.1/bin/julia -t 8 /home/mbonici/emulator-zoo/Effort.jl/velocileptors_lcdm/data_generation.jl
--- Effort.jl/velocileptors_lcdm/job_velocileptors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/mbonici/julia-1.9.1/bin/julia -t 8 /home/mbonici/emulator-zoo/Effort.jl/velocileptors_lcdm/effort_velocileptors_script.jl
