972bbc87a06adb7432c4b3898f8806762660cab2
marcobonici <bonici.marco@gmail.com>
2025-04-25 09:10:09 -0400

    Same as before...

--- Effort.jl/pybird_mnuw0wacdm/trainer_launcher.jl
+++ Effort.jl/pybird_mnuw0wacdm/trainer_launcher.jl
@@ -5,7 +5,7 @@ for ell in ells
     for component in components
         # Construct the bsub command
         bsub_command = `bsub -P c7 -q medium -o /home/mbonici/emulator-zoo/Effort.jl/pybird_mnuw0wacdm/job.out \
-                            -e /home/mbonici/EmulatorsTrainer.jl/scripts/job.err -n 8 -M 12000 \
+                            -e /home/mbonici/emulator-zoo/Effort.jl/pybird_mnuw0wacdm/job.err -n 8 -M 12000 \
                             -R'span[hosts=1] select[hname!=teo22 && hname!=infne01 && hname!=totem04 && hname!=totem07 && hname!=totem08 && hname!=geant15 && hname!=geant16 && hname!=aiace12 && hname!=aiace13 && hname!=aiace14 && hname!=aiace15 && hname!=aiace16 && hname!=aiace17]' \
                             /home/mbonici/julia-1.10.7/bin/julia -t 8 \
                             /home/mbonici/emulator-zoo/Effort.jl/pybird_mnuw0wacdm/trainer.jl \
