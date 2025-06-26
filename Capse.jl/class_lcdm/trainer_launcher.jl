c60eca78954667c4e090a42902fa3557f4e3e73b
marcobonici <bonici.marco@gmail.com>
2025-06-26 12:17:48 -0400

    Update trainer_launcher.jl

--- Capse.jl/class_lcdm/trainer_launcher.jl
+++ Capse.jl/class_lcdm/trainer_launcher.jl
@@ -4,7 +4,7 @@ output = "/farmdisk1/mbonici/trained_capse_lcdm_101"
 
 for spectrum in spectra
     # Construct the bsub command
-    folder_output = output * "_" * spectrum
+    folder_output = output
     bsub_command = `bsub -P c7 -q medium -o /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/job.out \
                         -e /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/job.err -n 4 -M 12000 \
                         -R'span[hosts=1] select[hname!=teo22 && hname!=infne01 && hname!=totem04 && hname!=totem07 && hname!=totem08 && hname!=geant15 && hname!=geant16 && hname!=aiace12 && hname!=aiace13 && hname!=aiace14 && hname!=aiace15 && hname!=aiace16 && hname!=aiace17]' \
