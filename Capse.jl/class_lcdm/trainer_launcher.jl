11a6c186bcdef11267c3b8d79f326a1efeba27bd
marcobonici <bonici.marco@gmail.com>
2025-06-26 12:17:09 -0400

    Update trainer_launcher.jl

--- Capse.jl/class_lcdm/trainer_launcher.jl
+++ Capse.jl/class_lcdm/trainer_launcher.jl
@@ -8,7 +8,7 @@ for spectrum in spectra
     bsub_command = `bsub -P c7 -q medium -o /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/job.out \
                         -e /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/job.err -n 4 -M 12000 \
                         -R'span[hosts=1] select[hname!=teo22 && hname!=infne01 && hname!=totem04 && hname!=totem07 && hname!=totem08 && hname!=geant15 && hname!=geant16 && hname!=aiace12 && hname!=aiace13 && hname!=aiace14 && hname!=aiace15 && hname!=aiace16 && hname!=aiace17]' \
-                        julia -t 4 --project=/home/mbonici/emulator-zoo/Capse.jl/class_mnuw0walcdm \
+                        julia -t 4 --project=/home/mbonici/emulator-zoo/Capse.jl/class_mnuw0wacdm \
                         /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/trainer.jl \
                         --spectrum $spectrum \
                         -i $input \
