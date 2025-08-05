fc64ac8c7a435c50c2f827be3179f50502d44dae
Marco Bonici <bonici.marco@gmail.com>
2025-08-06 00:56:49 +0200

    Update job_data_generation.sh

--- Capse.jl/axiclass/job_data_generation.sh
+++ Capse.jl/axiclass/job_data_generation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-julia --project=/home/mbonici/emulator-zoo/Capse.jl/class_mnuw0wacdm /home/mbonici/emulator-zoo/Capse.jl/class_lcdm/data_generation.jl
+julia --project=/home/mbonici/emulator-zoo/Capse.jl/axiclass /home/mbonici/emulator-zoo/Capse.jl/axiclass/data_generation.jl
