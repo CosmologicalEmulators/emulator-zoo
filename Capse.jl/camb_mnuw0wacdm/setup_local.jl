ec7900574fe4169123d90af3db1a62c3284550e5
marcobonici <bonici.marco@gmail.com>
2026-08-02 17:51:41 -0400

    Install glibc-compatible CAMB Conda packages

--- Capse.jl/camb_mnuw0wacdm/setup_local.jl
+++ Capse.jl/camb_mnuw0wacdm/setup_local.jl
@@ -6,6 +6,26 @@ Pkg.develop(PackageSpec(path=joinpath(repository_root, "EmulatorsTrainer.jl")))
 Pkg.resolve()
 Pkg.instantiate()
 
+using Conda
 using PyCall
+prefix = dirname(dirname(PyCall.python))
 python = PyCall.python
-run(`$python -m pip install camb scipy`)
+needs_repair = try
+    pyimport("camb")
+    pyimport("scipy")
+    false
+catch
+    true
+end
+if needs_repair
+    site_packages = strip(readchomp(`$python -c 'import site; print(site.getsitepackages()[0])'`))
+    for entry in readdir(site_packages; join=true)
+        name = basename(entry)
+        any(startswith(name, prefix) for prefix in ("camb", "numpy", "scipy", "sympy", "mpmath")) &&
+            rm(entry; recursive=true, force=true)
+    end
+    Conda.add(
+        ["camb", "numpy", "scipy", "sympy", "mpmath"], prefix;
+        channel="conda-forge", args=`--force-reinstall`,
+    )
+end
